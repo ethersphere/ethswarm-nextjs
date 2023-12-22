9c438aa07099e47aae5be892d34acad559d53768
Martin <martin.koterle@gmail.com>
2023-12-22 09:05:54 +0100

    Event changes

--- components/sections/EventPopup.tsx
+++ components/sections/EventPopup.tsx
@@ -38,16 +38,16 @@ const EventPopup = ({ image, title, content, cta }: EventPopupType) => {
       href={cta.href}
       onMouseEnter={() => setExpanded(true)}
       className={cx(
-        "relative mt-20 md:my-0 mx-6 md:fixed bottom-10 justify-end bg-[#F6F7F9] backdrop-blur-xl bg-opacity-80 left-0 right-0  transition-all duration-300  md:left-auto border border-opacity-20 border-[#D6DBDF] rounded-xl overflow-hidden p-1 md:right-8  z-40 flex group",
+        "relative mt-20 md:my-0 mx-6 md:fixed bottom-10 justify-between md:justify-end bg-[#F6F7F9] backdrop-blur-xl bg-opacity-80 left-0 right-0 transition-all duration-300 md:left-auto border border-opacity-20 border-[#D6DBDF] rounded-xl overflow-hidden p-1 md:right-8 z-40 flex group",
         expanded ? "max-w-lg" : "md:max-w-[154px]"
       )}
     >
       <div
         className={cx(
-          " px-4 pt-5 pb-3 mr-3 md:mr-0 text-[#141516] transition-all max-w-xs md:flex-shrink-0 "
+          "px-4 pt-5 pb-3 mr-3 md:mr-0 text-[#141516] transition-all max-w-xs md:flex-shrink-0"
         )}
       >
-        <h4 className=" text-[17px] leading-6">
+        <h4 className="text-[17px] leading-6">
           {"Next event: "}
           <b>{title}</b>
         </h4>
